b3c1f2a6be799abe464e81c2b0b34337bdb3a3b5
aslan <aslanindie02@gmail.com>
2021-02-16 23:36:10 -0800

    Added Highlighting func

---
+++ Assets/Scripts/SelectObjectMouseClick.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class SelectObjectMouseClick : MonoBehaviour
+{
+
+    public Material SelectingMat;
+
+    private Renderer SelectedRenderer;
+    private Material DefaultMat;
+    private Transform Location;
+
+   // Update is called once per frame
+    void Update()
+    {
+
+        if (Location != null)
+        {
+            SelectedRenderer = Location.gameObject.GetComponent<Renderer>();
+            DefaultMat = Location.gameObject.GetComponent<SelectableObject>().ReturnDefaultMat();
+            SelectedRenderer.material = DefaultMat;
+            Location = null;
+        }
+
+        RaycastHit HitInfo = new RaycastHit();
+        bool Hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out HitInfo);
+        if (Hit)
+        {
+            if(HitInfo.transform.gameObject.GetComponent<SelectableObject>() != null)
+            {
+                SelectedRenderer = HitInfo.transform.gameObject.GetComponent<Renderer>();
+                if ( SelectedRenderer != null)
+                {
+                    SelectedRenderer.material = SelectingMat;
+                }
+                Location = HitInfo.transform;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SelectableObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SelectableObject : MonoBehaviour
+{
+
+    private Material DefaultMat;
+
+    private void Start()
+    {
+        DefaultMat = GetComponent<Renderer>().material;
+    }
+
+    public Material ReturnDefaultMat()
+    {
+        return DefaultMat;
+    }
+}
