e6dd2e7e76e351d36f229bcd643d12f252a814b7
aslan <aslanindie02@gmail.com>
2021-02-17 00:57:53 -0800

    Added Movement

---
+++ Assets/Scripts/KeyboardMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class KeyboardMovement : MonoBehaviour
+{
+
+    public int RotateSpeed = -100;
+
+    public float Damping = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(Damping <= 1f)
+        {
+            Damping += 0.001f;
+        }
+        transform.Rotate(0, Input.GetAxis("Horizontal") * RotateSpeed * Time.deltaTime * Damping, 0);
+    }
+}
