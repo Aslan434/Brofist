9b1ed6e1d53ec0bad2e9951bd5495c5b048fb127
aslan <aslanindie02@gmail.com>
2021-02-18 04:21:56 -0800

    Added Manager

---
+++ Assets/Scripts/AddSelectableObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class AddSelectableObject : MonoBehaviour
+{
+
+    private GameObject Var;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] SelecatableObjs = GameObject.FindGameObjectsWithTag("SelectableObj");
+        foreach(GameObject SelecatableObj in SelecatableObjs)
+        {
+            Var = GameObject.Find(SelecatableObj.name);
+            Var.AddComponent<SelectableObject>();
+        }
+    }
+}
