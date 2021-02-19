8c1e2acf1969dc9463b45c9ee6c9bc982dcf0ba8
aslan <aslanindie02@gmail.com>
2021-02-19 07:03:43 -0800

    Added Roboto

--- Assets/Scripts/OptionsManage.cs
+++ Assets/Scripts/OptionsManage.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
 using static SelecatbleObjectStory;
+using UnityEngine.UI;
 
 public class OptionsManage : MonoBehaviour
 {
+
+    public GameObject Canvas;
+    public Text Text;
 
     private SelectableObject SelectedOBJ;
 
     public void ProcessOptions(Rply RawRply, string name)
     {
-        Debug.Log(RawRply);
+        Canvas.SetActive(true);
+        Text.text = RawRply.GenieTalk;
         SelectedOBJ = GameObject.Find(name).GetComponent<SelectableObject>();
         SelectedOBJ.enabled = false;
     }
