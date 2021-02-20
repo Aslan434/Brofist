54d10eb451a6af2615e7d700e6a22262b25f4ee9
aslan <aslanindie02@gmail.com>
2021-02-20 08:40:49 -0800

    ITS WORKING

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Intro : MonoBehaviour
+{
+
+    public GameObject UICanvas;
+    public SelectObjectMouseClick Script;
+
+
+    private void Start()
+    {
+        Script.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UICanvas.SetActive(false);
+            Script.enabled = true;
+        }
+    }
+}
