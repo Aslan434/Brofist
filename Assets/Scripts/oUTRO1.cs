b67937ba45062356949641f6320053cdc27d1604
aslan <aslanindie02@gmail.com>
2021-02-20 09:54:56 -0800

    Finished!

---
+++ Assets/Scripts/oUTRO1.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class oUTRO1 : MonoBehaviour
+{  
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Start", LoadSceneMode.Single);
+        }
+    }
+}
