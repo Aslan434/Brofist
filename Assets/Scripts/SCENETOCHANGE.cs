5c8f9b36f906c5ec7d1fd2e86da04a2f1e983057
aslan <aslanindie02@gmail.com>
2021-02-20 00:29:17 -0800

    Removed button

---
+++ Assets/Scripts/SCENETOCHANGE.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SCENETOCHANGE : MonoBehaviour
+{
+    public void ChangeSCENE(string Scene)
+    {
+        Debug.Log("dsf");
+        SceneManager.LoadScene(Scene, LoadSceneMode.Additive);
+    }
+}
