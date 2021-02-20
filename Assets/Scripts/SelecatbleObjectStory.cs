fed5c551dd8e5aa29ff8a7da0c6392d65ffe997e
aslan <aslanindie02@gmail.com>
2021-02-19 20:58:30 -0800

    Finished UI

--- Assets/Scripts/SelecatbleObjectStory.cs
+++ Assets/Scripts/SelecatbleObjectStory.cs
@@ -5,27 +5,18 @@ public class SelecatbleObjectStory : MonoBehaviour
     //Reshai
     public string Genie;
     public string DefaultRply;
-    public int Ui;
-    public string FirstOption;
-    public string SecondOption;
     Rply MyRply = new Rply();
 
     public struct Rply
     {
         public string GenieTalk;
         public string DefaultRplyByChar;
-        public int UI;
-        public string MyFirstOption;
-        public string MySecondOption;
     }
 
     private void Start()
     {
         MyRply.GenieTalk = Genie;
         MyRply.DefaultRplyByChar = DefaultRply;
-        MyRply.UI = Ui;
-        MyRply.MyFirstOption = FirstOption;
-        MyRply.MySecondOption = SecondOption;
     }
 
     public Rply ReturnCurrentRply()
