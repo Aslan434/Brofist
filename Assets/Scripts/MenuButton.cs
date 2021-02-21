423a457082de22fdf3ae0520fb3c00acba1ac212
aslan <aslanindie02@gmail.com>
2021-02-20 20:48:10 -0800

    Added show controls

--- Assets/Scripts/MenuButton.cs
+++ Assets/Scripts/MenuButton.cs
@@ -8,27 +8,36 @@ public class MenuButton : MonoBehaviour
     public Button Credits;
     public Button start;
     public Button Quit;
+    public Button Controls;
+    public GameObject ControlsText;
     public GameObject CreditsCanvas;
 
     private bool IsCreditsActive = false;
+    private bool IsControlsActive = false;
 
     void Start()
     {
         Quit.onClick.AddListener(QuitGame);
         start.onClick.AddListener(StartGame);
         Credits.onClick.AddListener(ShowCredits);
+        Controls.onClick.AddListener(ShowControls);
     }
 
     private void ShowCredits()
     {
         IsCreditsActive = !IsCreditsActive;
-        Debug.Log(IsCreditsActive);
         CreditsCanvas.SetActive(IsCreditsActive);
     }
 
     private void StartGame()
     {
         SceneManager.LoadScene("White", LoadSceneMode.Single);
+    }
+
+    private void ShowControls()
+    {
+        IsControlsActive = !IsControlsActive;
+        ControlsText.SetActive(IsControlsActive);
     }
 
     private void QuitGame()
